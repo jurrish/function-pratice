e1141516cf3405353ae76abf3404ee91df40ee2e
Frank Iriarte <jurrish@gmail.com>
2016-03-18 09:25:08 -0700

    finished first problem

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('javascript works');
+
+function sum(numOne, numTwo){
+  var total = (numOne + numTwo);
+  console.log('the sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
+}
