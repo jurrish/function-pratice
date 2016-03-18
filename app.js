778fc399d4e0e527ebf755d05ba052aeaf151743
Frank Iriarte <jurrish@gmail.com>
2016-03-18 10:56:20 -0700

    did 3rd problem, added returns to p1 and p2

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ console.log('javascript works');
 //p1
 function sum(numOne, numTwo){
   var total = (numOne + numTwo);
-  console.log('the sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
+  return total;
+  //console.log('the sum of ' + numOne + ' and ' + numTwo + ' is ' + total + '.');
 }
 
 //p2
 function multiply(numOne, numTwo){
   var multiply = (numOne * numTwo);
-  console.log('The product of ' + numOne + ' and ' + numTwo + ' is ' + multiply);
+  return multiply;
+  //console.log('The product of ' + numOne + ' and ' + numTwo + ' is ' + multiply);
+}
+
+//p3
+function sumAndMultiply(numOne, numTwo, numThree){
+  var newSum = sum(numOne, numTwo);
+  var lastSum = sum(newSum, numThree);
+  var newMult = multiply(numOne, numTwo);
+  var lastMult = multiply(newMult, numThree);
+  console.log(numOne + ' and ' + numTwo + ' and ' + numThree + ' sum to ' + lastSum);
+  console.log(numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + lastMult);
+  return [lastSum, lastMult];
 }
